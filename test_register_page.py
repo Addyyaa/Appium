f5dfb0ce22708dda6936a7b28839659968cdf016
SHEN FENG <2698567570@qq.com>
2023-12-11 22:56:42 +0800

    pytest模块下不能有init构造函数，否则无法识别测试函数。 不能使用self调用变量

--- test_register_page.py
+++ test_register_page.py
@@ -3,11 +3,15 @@ import Element
 import Config
 import pytest
 import logging
-class TestRegisterPage:
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self, request):
-        # 初始化 appium 连接操作
+import VersionSelection
+
+
+class TestChineseRegisterPage:
+    @pytest.fixture(scope="class", autouse=True)
+    def setup(self):
         driver = Init.get_driver()
+        version = VersionSelection.VersionSelection(driver)
+        version.version_selection("Chinese", "Chinese")
         element = Element.Element_version
         # 账号及密码
         email = Config.Config.email
@@ -18,6 +22,8 @@ class TestRegisterPage:
         # TODO 需要对验证码重新设计，验证码输入、不输入、没点击获取验证码
         verify_code = Config.Config.verify_code
         logger = logging.getLogger(__name__)
+        # 进入注册界面
+        driver.find_element(by='xpath', value=element.Ch_LoginPage_Register).click()
         yield {
             "driver": driver,
             "element": element,
@@ -29,12 +35,25 @@ class TestRegisterPage:
             "verify_code": verify_code,
             "logger": logger
         }
-        # 打印yield返回值
         driver.quit()
 
-    # 正常注册流程
-    def test_regist_successfull(self, setup):
-        data = setup.values()
-        print(data)
+    # 中文国内手机注册流程
+
+    def test_chlanguage_chregion_phone_regist(self, setup):
+        setup["driver"].find_element(by='xpath', value=setup["element"].Ch_Phone_Register_AreaCodeList).click()
+        setup["driver"].find_element(by='xpath', value=setup["element"].Ch_Phone_Register_AreaCode_Search).send_keys("中国大陆")
+        setup["driver"].implicitly_wait(1)
+        setup["driver"].find_element(by='xpath', value=setup["element"].Ch_Area_Code_86).click()
+        setup["driver"].find_element(by='xpath', value=setup["element"].Ch_Phone_Register_PhoneNumber).send_keys(setup["phone"])
+
+    def test_ppt(self):
+        print("1")
 
 
+
+
+# class TestEnglishRegisterPage:
+#     def __init__(self):
+#         self.driver = Init.get_driver()
+#         self.version = VersionSelection.VersionSelection(self.driver)
+#         self.version.version_selection("Chinese", "English")
